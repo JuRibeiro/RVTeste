0d5022872f60447a0d5d36f462a2bd674302db31
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-12-13 21:52:29 -0300

    Subindo teste RV

---
+++ arquivo.js
@@ -0,0 +1,7 @@
+//consumo API
+const url = "https://front-br-challenges.web.app/api/v2/green-thumb/?sun=high&water=regularly&pets=false";
+
+// get elements
+var sun = document.getElementById("sun");
+var water = document.getElementById("water");
+var pets = document.getElementById("pets");
